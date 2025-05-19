5ed8246b6f1c9f938a61fdf6ddc5003ec53845f0
Jeuk0908 <cell4086@naver.com>
2025-05-19 18:36:40 +0900

    initial commit

---
+++ src/main/java/org/example/prumpt_be/PrumptBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.prumpt_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrumptBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrumptBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/prumpt_be/PrumptBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.prumpt_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrumptBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
