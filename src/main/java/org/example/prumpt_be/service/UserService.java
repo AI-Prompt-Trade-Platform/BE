b49294868880c088bba3e933fc4df62c4b0cdd6d
chocozep <cozep9954@naver.com>
2025-06-12 17:00:14 +0900

    포인트 결제 코드 수정

---
+++ src/main/java/org/example/prumpt_be/service/UserService.java
@@ -0,0 +1,22 @@
+package org.example.prumpt_be.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.prumpt_be.dto.entity.Users;
+import org.example.prumpt_be.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public void addPointToUser(Long userId, int amount) {
+        Users user = userRepository.findById(String.valueOf(userId))
+                .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다"));
+        user.setPoint(user.getPoint() + amount);
+        userRepository.save(user);
+    }
+
+}
